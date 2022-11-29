c856decda556e186c6119b9e70f3678dc949628a
MarshallLiu-Dev <paulomartins12358@gmail.com>
2022-11-29 19:21:26 -0300

    frist commit

---
+++ fragrancias.js/cheiros.js
@@ -0,0 +1,38 @@
+var perfumes = [
+    { 
+        nome: "Malbec",
+        fabricante: "Boticario",
+        tipo:"Amadeirado",
+},
+    { 
+    nome: "Lilly",
+    fabricante: "Boticario",
+    tipo:" Floral Amadeirado",
+},
+    { 
+    nome: "Essencial",
+    fabricante: "Natura",
+    tipo:"Floral Elegante",
+},
+    { 
+    nome: "Puro Vudol",
+    fabricante: "Mae do Cris",
+    tipo:"Suave",
+},
+    { 
+    nome: "Brisa do Deserto",
+    fabricante: "Simpsons",
+    tipo:"Floral Suave",
+},
+    { 
+    nome: "Far Away Glamour",
+    fabricante: "Avon",
+    tipo:"Suave",
+},
+    { 
+    nome: "Glamour Secrets Black Desodorant",
+    fabricante: "Avon",
+    tipo:"Suave",
+}
+];
+
